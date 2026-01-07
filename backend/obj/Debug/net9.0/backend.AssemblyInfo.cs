9eaaadf164c0307dc1d83e6c50b6f3764767c03b
amc1988 <9002amc@gmail.com>
2026-01-07 15:54:34 +0100

    fix(dev): add dev-server proxy to route /api to backend

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f0294ba665a53dde5f14664f76b0fe505b0e4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c40e3313cf6e58c099a3b485c96f734b22d2481d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
